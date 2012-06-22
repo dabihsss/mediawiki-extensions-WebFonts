a5044c4ef054393a92b722b26b5cb5e36bd43c43
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2012-06-22 18:18:59 +0530

    Update Amiri font to 1.0.2 version
    Ref http://www.khaledhosny.org/node/194
    
    Change-Id: I44c729feb31ecb292348cd4ea7101f27bf7d60cd

--- resources/ext.webfonts.fontlist.js
+++ resources/ext.webfonts.fontlist.js
@@ -11,7 +11,7 @@
 				eot: "Arab/amiri-regular.eot",
 				ttf: "Arab/amiri-regular.ttf",
 				woff: "Arab/amiri-regular.woff",
-				version: "1.0"
+				version: "1.0.2"
 			},
 
 			"Iranian Sans": {
